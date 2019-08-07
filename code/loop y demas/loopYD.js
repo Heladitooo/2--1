3f55e866bf9edaa608d61aa2427dd61a5204d988
Jose Padron F <josepadron.go@gmail.com>
2019-08-07 01:41:05 -0500

    fixing some indent errors

--- code/loop y demas/loopYD.js
+++ code/loop y demas/loopYD.js
@@ -1,16 +1,14 @@
-function Dvinci()
-{
+function Dvinci() {
   canvas.width = document.body.clientWidth;
   canvas.height = document.body.clientHeight - 40;
-  lienzo.clearRect(0,0,canvas.width,canvas.height)
+  lienzo.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function loop()
-{
+function loop() {
   Dvinci();
   mover();
 
   salirCamiones();
 }
 
-setInterval(loop,10)
+setInterval(loop, 10);

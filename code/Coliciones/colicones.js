999a846515e9c1f7cce4e2a816155021a82d14dc
Heladito <nicolasestevanprieto@gmail.com>
2019-08-06 20:01:39 -0500

    Mas sintaxis

--- code/Coliciones/colicones.js
+++ code/Coliciones/colicones.js
@@ -1,5 +1,14 @@
 function coliciones()
 {
+  //movXp1 = posicion en x del jugador 1
+  //movYp1 = posicion en y del jugador 1
+
+  //camionPosXY = posicion x de los camiones que van hacia arriva y abajo
+  //camionPosYY = posicion Y de los camiones que van hacia arriva y abajo
+
+  //camionPosXX = posicion x de los camiones que van hacia derecha y izquierda
+  //camionPosYX = posicion Y de los camiones que van hacia derecha y izquierda
+
   if( movXp1  >= camionPosXY &&  movXp1 +  20 <= camionPosXY + camionY.ancho && movYp1  >=  camionPosYY && movYp1 + 20 <= camionPosYY + camionY.alto )
   {
     debugger
